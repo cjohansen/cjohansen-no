24ac9e11ae8ed786f614a6c63749809a7078196a
Christian Johansen <christian@cjohansen.no>
2019-09-20 22:40:07 +0200

    Configure deps.edn

---
+++ dev/cjohansen_no/dev.clj
@@ -0,0 +1,31 @@
+(ns cjohansen-no.dev
+  (:require [cjohansen-no.web :as web]
+            [clojure.tools.namespace.repl :as repl]
+            [mount.core :as mount :refer [defstate]]
+            [prone.middleware :as prone]
+            [ring.adapter.jetty :as jetty]))
+
+(defstate server
+  :start (jetty/run-jetty
+          (-> #'web/app
+              prone/wrap-exceptions)
+          {:port 3030
+           :join? false})
+  :stop (.stop server))
+
+(defn start []
+  (mount/start))
+
+(defn stop []
+  (mount/stop))
+
+(defn restart []
+  (stop)
+  (repl/refresh :after 'cjohansen-no.dev/start))
+
+(defn -main [& args]
+  (start))
+
+(comment
+  (start)
+)
