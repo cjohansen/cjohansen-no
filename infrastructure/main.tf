8584dbb7de08000885233ae62740a13f7249bd96
Christian Johansen <christian@cjohansen.no>
2019-12-11 07:17:37 +0100

    Oops, add the domain

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -158,6 +158,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   is_ipv6_enabled = true
   comment = "${local.app_name} distribution"
   default_root_object = "index.html"
+  aliases = ["${local.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
